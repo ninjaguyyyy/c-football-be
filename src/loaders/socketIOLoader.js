27ca1398be2738104972275a0ef5f4a9d51006a9
nguyenhuuchi3006 <ninjaguyyyy@gmail.com>
2022-03-28 17:29:58 +0700

    feat: handle online user

--- src/loaders/socketIOLoader.js
+++ src/loaders/socketIOLoader.js
@@ -1,16 +1,40 @@
 const { Server } = require('socket.io');
 
+let onlineUsers = [];
+
 module.exports = (httpServer) => {
   const io = new Server(httpServer, {
     /* options */
   });
 
   io.on('connection', (socket) => {
-    console.log('A main socket client connected');
+    console.log('A main socket client connected', socket.id);
 
     setInterval(() => {
       socket.emit('test', Math.random());
     }, 5000);
+  });
+
+  io.of('/online-users').on('connection', (socket) => {
+    socket.on('online', (user) => {
+      onlineUsers.push({ user, socketId: socket.id });
+      socket.broadcast.emit('new', user);
+    });
+
+    socket.on('disconnect', () => {
+      console.log('a user disconnected!');
+      const user = onlineUsers.find(({ socketId }) => socketId === socket.id)?.user;
+
+      socket.broadcast.emit('remove', user);
+      onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+    });
+
+    socket.on('get-list', () => {
+      socket.emit(
+        'list',
+        onlineUsers.filter(({ socketId }) => socketId !== socket.id).map(({ user }) => user)
+      );
+    });
   });
 
   io.of('/chat').on('connection', (socket) => {
